50d39534cef977b55fc8973b65a60158f5dc5df5
Oleksandr Afanasiev <afanasievov@gmail.com>
2018-05-12 15:18:48 +0300

    Add a Button component. Add buttons to OrderSummary.

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 import Aux from '../../../hoc/Aux';
+import Button from '../../UI/Button/Button';
 
-const orderSummary = ({ ingredients }) => {
-  const ingredientSummary = Object.entries(ingredients).map(([key, value]) => (
+const orderSummary = (props) => {
+  const ingredientSummary = Object.entries(props.ingredients).map(([key, value]) => (
     <li key={key}>
       <span style={{ textTransform: 'capitalize' }}>{key}</span>: {value}
     </li>
@@ -14,6 +15,12 @@ const orderSummary = ({ ingredients }) => {
       <p>A delicious burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
       <p>Continue to Checkout?</p>
+      <Button btnType="Danger" clicked={props.purchaseCancelled}>
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>
+        CONTINUE
+      </Button>
     </Aux>
   );
 };
