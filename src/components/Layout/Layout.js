31c25674998e7fef571b55fa4b288e9c1060e4b0
Oleksandr Afanasiev <afanasievov@gmail.com>
2018-05-13 14:19:42 +0300

    Make Logo responsive.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Aux from '../../hoc/Aux';
-import classes from './Layoyt.css';
+import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
