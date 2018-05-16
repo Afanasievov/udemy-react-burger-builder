21bf37da62bd7bb0cee22228f2d19186291d509b
Oleksandr Afanasiev <afanasievov@gmail.com>
2018-05-16 08:43:03 +0300

    Handle requests errors.

---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+import Modal from '../../components/UI/Modal/Modal';
+import Aux from '../Auxiliary/Aux';
+
+const withErrorHandler = (WrappedComponent, axios) =>
+  class extends Component {
+    state = {
+      error: null,
+    };
+
+    componentDidMount() {
+      axios.interceptors.request.use((req) => {
+        this.setState({ error: null });
+        return req;
+      });
+      axios.interceptors.response.use((res) => res, (error) => this.setState({ error }));
+    }
+
+    errorConfirmedHandler = () => {
+      this.setState({ error: null });
+    };
+    render() {
+      return (
+        <Aux>
+          <Modal show={this.state.error} modalClosed={this.errorConfirmedHandler}>
+            {this.state.error ? this.state.error.message : null}
+          </Modal>
+          <WrappedComponent {...this.props} />
+        </Aux>
+      );
+    }
+  };
+
+export default withErrorHandler;
