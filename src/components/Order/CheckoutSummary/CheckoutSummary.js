0d3acaad7307b65a3fb0a0e89fe4e663716d6aa9
Oleksandr Afanasiev <afanasievov@gmail.com>
2018-05-24 22:30:19 +0300

    Add the Checkout Container

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
+import classes from './CheckoutSummary.css';
+
+const checkoutSummary = (props) => (
+  <div className={classes.CheckoutSummary}>
+    <h1>We hope it tastes well!</h1>
+    <div style={{ width: '100%', height: '300px', margin: 'auto' }}>
+      <Burger ingredients={props.ingredients} />
+    </div>
+    <Button btnType="Danger">CANCEL</Button>
+    <Button btnType="Success">SUCCESS</Button>
+  </div>
+);
+
+export default checkoutSummary;
